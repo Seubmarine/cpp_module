70212f9607a4ebda068d9d4477e39672dae39fbf
Seubmarine <seubmarinepro@gmail.com>
2023-01-24 01:01:27 +0100

    fix include and timestamp is correct

--- cpp0/ex02/Account.cpp
+++ cpp0/ex02/Account.cpp
@@ -1,9 +1,28 @@
 #include "Account.hpp"
 #include <iostream>
 #include <ctime>
+#include <iomanip>
+
+int Account::_nbAccounts;
+int	Account::_totalAmount;
+int	Account::_totalNbDeposits;
+int	Account::_totalNbWithdrawals;
+
+void	Account::_displayTimestamp( void ) {
+    time_t raw_time;
+    std::time(&raw_time);
+    tm *clean_time = gmtime(&raw_time);
+    std::clog << "[" << clean_time->tm_year + 1900
+    << std::setfill('0') << std::setw(2) << (clean_time->tm_mon + 1) 
+    << std::setfill('0') << std::setw(2) << clean_time->tm_mday << "_" 
+    << std::setfill('0') << std::setw(2) << (clean_time->tm_hour + 1) % 24
+    << std::setfill('0') << std::setw(2) << clean_time->tm_min
+    << std::setfill('0') << std::setw(2) << clean_time->tm_sec
+    <<"] ";
+}
 
 int	Account::getNbAccounts( void ) {
-    return (Account::_nbAccounts);
+    return (_nbAccounts);
 }
 int	Account::getTotalAmount( void ) {
     return (_totalAmount);
@@ -32,9 +51,13 @@ Account::Account( int initial_deposit ) {
     std::cout << "index:" << this->_accountIndex << ";ammount:" << this->_amount << ";created" << std::endl;
 }
 
+Account::~Account(void) {
+    std::cout << "Destroyed:" << this->_accountIndex << std::endl;
+}
+
 void	Account::makeDeposit( int deposit ) {
+    this->_amount += deposit;    
     this->_nbDeposits += 1;
-    this->_nbWithdrawals 
 }
 bool	Account::makeWithdrawal( int withdrawal ) {
     if (this->_amount - withdrawal >= 0) //Can withdrawl
@@ -42,14 +65,16 @@ bool	Account::makeWithdrawal( int withdrawal ) {
         this->_amount -= withdrawal;
         this->_nbWithdrawals += 1;
         Account::_totalNbWithdrawals += 1;
+        return (true);
     }
+    //can't withdraw
+    return (false);
 }
-	int		checkAmount( void ) const;
-	void	displayStatus( void ) const {
-        
-    }
 
+int		Account::checkAmount( void ) const {
+    return (42);
+}
 
-static void	_displayTimestamp( void ) {
-    std::clog << "[" << std::time(NULL) << "] ";
+void	Account::displayStatus( void ) const {
+    // Account::_displayTimestamp();
 }
