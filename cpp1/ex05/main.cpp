fcdf7c15a1d3720993ea2c6261995afaeed2258e
Seubmarine <seubmarinepro@gmail.com>
2023-01-25 03:23:08 +0100

    cpp1 ex05

---
+++ cpp1/ex05/main.cpp
@@ -0,0 +1,14 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("NOT_A_COMPLAINT");
+	harl.complain("debug");
+	return 0;
+}
