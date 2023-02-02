4edc38575222a2c0d58588bea778a7619f10d893
tbousque <tbousque@student.42.fr>
2023-02-02 14:53:53 +0100

    cpp1 ex01 add missing zombieHorde function and clean test in main

--- cpp1/ex01/main.cpp
+++ cpp1/ex01/main.cpp
@@ -16,19 +16,32 @@ void new_horde_announce_live(int horde_size, std::string horde_name) {
 	}
 }
 
-int main(void)
-{	
-	new_horde_announce_die(3, "Mary");
-	// new_horde_announce_die(-1, "invalide"); //Watch out for passing negative number to new[] will abort the program
-	// new_horde_announce_live(5, "Will"); // Watch out to delete correctly, should leak
-	
+void test_zero_sized_zombie_horde() {
 	//using zero sized array, see: https://stackoverflow.com/a/1087066/17480217
 	{
-		Zombie *zero_size_horde = zombieHorde(-1, "Invalid");
+		Zombie *zero_size_horde = zombieHorde(0, "Invalid");
 		// zero_size_horde[0].setName("Zero");
-		// zero_size_horde[0].announce();
-		
-		(void)zero_size_horde;
+		// zero_size_horde[0].announce(); // shouldn't access member or call function
 		delete[] zero_size_horde; //even if the allocated array is of size 0 you should always call delete on it
 	}
+}
+
+void test_leak() {
+	new_horde_announce_live(5, "Will");
+}
+
+void test_bad_alloc() {
+	new_horde_announce_die(-1, "invalide");
+}
+
+void test_ok() {
+	new_horde_announce_die(3, "Mary");
+}
+
+int main(void)
+{	
+	test_ok();
+	// test_leak(); // Watch out to delete correctly, should leak
+	// test_bad_alloc(); //Watch out for passing negative number to new[] will abort the program
+	// test_zero_sized_zombie_horde(); // a horde of size zero
 }
