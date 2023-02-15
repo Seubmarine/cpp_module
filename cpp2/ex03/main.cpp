f26b1241df8da825902785950b53b4d25aaa4fea
tbousque <tbousque@student.42.fr>
2023-02-15 14:01:35 +0100

    cpp2 ex03 added test case negative triangle

--- cpp2/ex03/main.cpp
+++ cpp2/ex03/main.cpp
@@ -5,13 +5,16 @@
 void bsp_verbose(Point a, Point b, Point c, Point point) {
   std::cout << "Forming a triangle from :\nA" << a << "\nB" << b << '\n'
             << 'C' << c << "\n\nPoint" << point << "\n\n";
-  std::cout << "Is the point inside the triangle: " << bsp(a, b, c, point)
+  std::cout << "Is the point inside the triangle: " << (bsp(a, b, c, point) ? "true" : "false")
             << std::endl;
 }
 
 int main(void) {
-  bsp_verbose(Point(101.0f, 103.0f), Point(103.0f, 103.f),
-              Point(102.0f, 101.0f), Point(102.0f, 103.0f));
-  bsp_verbose(Point(101.0f, 103.0f), Point(103.0f, 103.f),
-              Point(102.0f, 101.0f), Point(104.0f, 103.0f));
+	bsp_verbose(Point(101.0f, 103.0f), Point(103.0f, 103.f),
+            	Point(102.0f, 101.0f), Point(102.0f, 103.0f));
+	bsp_verbose(Point(101.0f, 103.0f), Point(103.0f, 103.f),
+            	Point(102.0f, 101.0f), Point(102.0f, 104.0f));
+	bsp_verbose(Point(-4, -1), Point(-1, -3),
+            	Point(-10, -8), Point(-8, -6));
+
 }
