3e1c93d2ac03aab8d4acb53a04dffd7200d81d3a
tbousque <tbousque@student.42.fr>
2023-02-28 19:25:21 +0100

    add test case

--- cpp4/ex02/main.cpp
+++ cpp4/ex02/main.cpp
@@ -4,6 +4,32 @@
 #include "Dog.hpp"
 
 #include <iostream>
+
+void ex01()
+{
+    const Animal *i = new Dog();
+    const Animal *j = new Cat();
+
+    delete j;
+    delete i;
+
+    Dog dog1;
+    {
+        Dog dog2(dog1);
+    }
+
+    Cat cat1;
+    {
+        Cat cat2(cat1);
+    }
+
+
+    Animal *animals[] = {new Dog(dog1), new Cat(cat1), new Dog(Dog()), new Cat()};
+    for (size_t i = 0; i < sizeof(animals) / sizeof(animals[0]); i++) {
+        delete animals[i];
+    }
+}
+
 int main()
 {
     Dog dog1;
@@ -22,5 +48,7 @@ int main()
     Animal *test1 = new Cat;
     test1->makeSound();
     delete test1;
-    return 0;
+	ex01();
+    
+	return 0;
 }
