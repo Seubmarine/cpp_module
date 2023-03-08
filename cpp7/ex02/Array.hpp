896344b148892f89bd0a9ade1d24434665b0c2d0
tbousque <tbousque@student.42.fr>
2023-03-08 12:23:33 +0100

    cpp7 ex02

---
+++ cpp7/ex02/Array.hpp
@@ -0,0 +1,60 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+#include <stddef.h>
+#include <exception>
+
+template<typename T>
+class Array
+{
+private:
+	T *elements;
+	size_t array_size;
+public:
+	
+	Array() : elements(NULL) , array_size(0) {};
+	
+	Array(unsigned int size) : elements(NULL) , array_size(size) {
+		this->elements = new T[this->array_size]();
+	}
+	
+	~Array() {
+		if (this->elements != NULL) {
+			delete[] elements;
+			array_size = 0;
+		}
+	}
+
+	Array(const Array &rhs) {
+		this->elements = NULL;
+		this->array_size = 0;
+		if (rhs.size() > 0) {
+			*this = rhs;
+		}
+	}
+
+	const Array &operator=(const Array &rhs) {
+		if (this != &rhs) {
+			if (this->elements != NULL)
+				delete[] this->elements;
+			this->array_size = rhs.array_size;
+			this->elements = new T[this->array_size]();
+			for (size_t i = 0; i < this->array_size; i++) {
+				this->elements[i] = rhs.elements[i];
+			}
+		}
+		return *this;
+	}
+
+	T& operator[](int idx) const {
+		if (this->array_size == 0 || idx < 0 || size_t(idx) > this->array_size - 1)
+			throw std::exception();
+		return this->elements[idx];
+	};
+
+	size_t size() const {
+		return this->array_size;
+	};
+
+};
+
+#endif
---
+++ cpp7/ex02/main.cpp
@@ -0,0 +1,105 @@
+#include "Array.hpp"
+#include <iostream>
+#include <stdlib.h> // srand
+void mytest() {
+	Array<int> test(10);
+	
+	std::cout << "Show original array test" << std::endl;
+	for (size_t i = 0; i < test.size(); i++)
+	{
+		std::cout << test[i] << ' ';
+	}
+	std::cout << std::endl;
+
+	std::cout << "modify array member using i * i" << std::endl;
+	for (size_t i = 0; i < test.size(); i++)
+	{
+		test[i] = i * i;
+		std::cout << test[i] << ' ';
+	}
+	std::cout << std::endl;
+
+	std::cout << "Copy test as test2" << std::endl;
+	std::cout << "modify test2 array member using test2[i] * test2[i]" << std::endl;
+	Array<int> testcopy(test);
+	for (size_t i = 0; i < testcopy.size(); i++)
+	{
+		testcopy[i] = testcopy[i] * testcopy[i];
+		std::cout << testcopy[i] << ' ';
+	}
+	std::cout << std::endl;
+	
+	std::cout << "Show original array test to verify if any member was changed by modifying test2" << std::endl;
+	for (size_t i = 0; i < test.size(); i++)
+	{
+		std::cout << test[i] << ' ';
+	}
+	std::cout << std::endl;
+
+
+	std::cout << "An array of float" << std::endl;
+	Array<float> testf(10);
+	for (size_t i = 0; i < testf.size(); i++)
+	{
+		testf[i] = i * 0.1f;
+		std::cout << testf[i] << ' ';
+	}
+	std::cout << std::endl;
+}
+
+#define MAX_VAL 750
+int test_original()
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
+
+int main(void)
+{
+	mytest();
+	return test_original();
+}
