5ac4869b861eb6d3a25ea99a5ad3c3d85587e666
tbousque <tbousque@student.42.fr>
2023-03-10 13:47:17 +0100

    add test

--- cpp8/ex01/main.cpp
+++ cpp8/ex01/main.cpp
@@ -1,6 +1,8 @@
 #include "Span.hpp"
 #include <iostream>
 
+#define array_sizeof(x) (sizeof(x) / sizeof(x[0]))
+
 void test_original() {
 	Span sp = Span(5);
 	sp.addNumber(6);
@@ -20,9 +22,57 @@ void test_addnumber_iterator() {
 	std::cout << sp.longestSpan() << std::endl;
 }
 
+void test_negative() {
+	int tmp[] = {-5, -10, -25, -18, 1};
+	Span sp = Span(array_sizeof(tmp));
+	sp.addNumbers(tmp, tmp + array_sizeof(tmp));
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+}
+
+void test_exception() {
+	try
+	{
+		Span sp(4);
+		for (size_t i = 0; i < 10; i++)
+		{
+			sp.addNumber(0);
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Span sp(4);
+		sp.addNumber(42);
+		sp.shortestSpan();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Span sp(4);
+		sp.shortestSpan();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
 int main()
 {
+	std::cout << "test original" << std::endl;
 	test_original();
+	std::cout << "test iter" << std::endl;
 	test_addnumber_iterator();
+	std::cout << "test negative" << std::endl;
+	test_negative();
+	std::cout << "test exception" << std::endl;
+	test_exception();
 	return 0;
 }
