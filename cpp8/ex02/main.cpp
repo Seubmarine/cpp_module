ae29dc0afaf2634bd434a4de31d789be565ac683
tbousque <tbousque@student.42.fr>
2023-03-10 14:04:50 +0100

    cpp8 ex02 add some test

--- cpp8/ex02/main.cpp
+++ cpp8/ex02/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <stack>
 
-int main() {
+void test_original() {
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -26,10 +26,36 @@ int main() {
 		std::cout << *it << std::endl;
 		++it;
 	}
-	mstack.pop();
-	mstack.pop();
+
 	std::cout << "top: " << mstack.top() << std::endl;
 	std::stack<int> s(mstack);
 	MutantStack<int> c(mstack);
+	mstack.pop();
+	mstack.pop();
+
+	std::cout << "print copy after two pop or original" << std::endl;
+	it = c.begin();
+	ite = c.end();
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::cout << "size: " << c.size() << std::endl;
+	std::cout << "isempty: " << c.empty() << std::endl;
+	std::cout << "isempty after " << c.size() << " pop: " << std::endl;
+	size_t c_size = c.size();
+	for (size_t i = 0; i < c_size; i++)
+	{
+		c.pop();
+	}
+	std::cout << c.empty() << std::endl;
+	
+
+}
+
+int main() {
+	test_original();
 	return 0;
 }
